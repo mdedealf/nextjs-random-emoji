0fe5aacf6c377f369caead6804b0e1c290bc966b
mdedealf <dede.alfaruqq@gmail.com>
2024-10-09 11:42:47 +0700

    feat: adding feature shuffle emoji and caption

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,10 @@ const RootLayout: FC<RootLayoutProps> = ({ children }) => {
   return (
     <html lang="en">
       <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Emojilogy</title>
+
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,61 @@
-import { FC } from "react";
+"use client";
+import { FC, useEffect, useState } from "react";
 import Image from "next/image";
 
+interface Emojis {
+  name: string;
+  category: string;
+  group: string;
+  htmlCode: string[];
+  unicode: string[];
+}
+
 const HomePage: FC = () => {
+  const [emojis, setEmojis] = useState<Emojis[]>([]);
+  const [currentEmoji, setCurrentEmoji] = useState<string>("&#129402;");
+  const [currentCaption, setCurrentCaption] = useState<string>(
+    "You&apos;re sad and you know it. just give up, don&apos;t try."
+  );
+
+  const shuffleEmojis = () => {
+    if (emojis.length > 0) {
+      const randomIdx = Math.floor(Math.random() * emojis.length);
+      const randomEmoji = emojis[randomIdx];
+
+      const shuffleInterval = setInterval(() => {
+        const randomShuffleIdx = Math.floor(Math.random() * emojis.length);
+        setCurrentEmoji(emojis[randomShuffleIdx].htmlCode[0]);
+      }, 100);
+
+      setTimeout(() => {
+        clearInterval(shuffleInterval);
+
+        setCurrentEmoji(randomEmoji.htmlCode[0]);
+        setCurrentCaption(`${randomEmoji.category} ${randomEmoji.name}`);
+      }, 2000);
+    }
+  };
+
+  useEffect(() => {
+    const fetchEmojis = async () => {
+      try {
+        const response = await fetch(
+          "https://emojihub-1001447344924.asia-southeast2.run.app/api/all"
+        );
+        const data = (await response.json()) as Emojis[];
+        setEmojis(data);
+
+        // Shuffle emoji when emoji fetched
+        shuffleEmojis();
+      } catch (error) {
+        throw new Error("Something went wrong");
+        console.log(error);
+      }
+    };
+
+    fetchEmojis();
+  }, []);
+
   return (
     <section className="flex flex-col bg-[#FAF4E1] h-screen min-w-[375px] max-w-[430px] p-[24px]">
       <Image
@@ -11,6 +65,7 @@ const HomePage: FC = () => {
         width={100}
         height={26}
       />
+
       <div className="flex flex-col justify-evenly items-center bg-[#F8DD84] h-full rounded-[2px]">
         <div className="text-[#4C9BB9] text-[40px] font-bold text-center w-[300px]">
           What&#39;s your <span className="text-[#1C5469]">emoji</span> today?
@@ -19,19 +74,18 @@ const HomePage: FC = () => {
           <span className="italic text-[#4A7582] text-[20px] font-[400px] leading=[26px] mb-[14px]">
             Click it!
           </span>
-          <button>
-            <Image
-              className="w-auto h-[160px]"
-              src="/images/pleading-face.svg"
-              alt="Pleading face img"
-              width={160}
-              height={160}
+          <button onClick={shuffleEmojis} className="h-[160px] w-[160px]">
+            <span
+              className="text-[120px] max-h-[120px] max-w-[120px]"
+              dangerouslySetInnerHTML={{ __html: currentEmoji }}
             />
           </button>
         </div>
-        <span className="text-[#4A7582] text-[14px] w-[180px] text-center italic">
-          You&#39;re sad and you know it. just give up, don&#39;t try.
-        </span>
+
+        <span
+          className="text-[#4A7582] text-[16px] w-[180px] text-center italic"
+          dangerouslySetInnerHTML={{ __html: currentCaption }}
+        />
       </div>
     </section>
   );
